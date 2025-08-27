9739d748db5dde4bc5d0df05fb6accd10fc9189f
kiran raveendran <kiranraveendran50@gmail.com>
2025-08-27 21:39:38 +0530

    stop tracking db.sqlite3 and added to .gitignore, add patch method to patientRegisterAPI with serializer validation

--- Backend/clinical_panel_app/urls.py
+++ Backend/clinical_panel_app/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path("patients/register/", PatientRegisterAPI.as_view(), name="patient-register"),
+    path("patients/edit/<int:patient_id>/", PatientRegisterAPI.as_view(), name="patient-edit"),
     path("delete_users/<int:pk>/", UserDeleteAPI.as_view(), name="user-delete"),
 
 ]
